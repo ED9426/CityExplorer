8bd151bda4cec3ae67240902d9c13a6132620cca
huigeng97 <hgeng7@wisc.edu>
2021-03-19 18:04:51 -0500

    new C

--- FrontEndDeveloperTests.java
+++ FrontEndDeveloperTests.java
@@ -1,9 +1,6 @@
-import java.io.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FrontEndDeveloperTests {
 
