c9946d1d5ba438f511a3cc552672f7310ddcc84e
Edward Zhao <77092154+ED9426@users.noreply.github.com>
2021-09-27 00:43:42 -0500

    Add files via upload

--- FrontendInterface.java
+++ FrontendInterface.java
@@ -1,12 +1,11 @@
 // --== CS400 File Header Information ==--
-// Name: Gahan Sudhir
-// Email: gsudhir@wisc.edu
+// Name: HUI GENG
+// Email: hgeng7@wisc.edu
 // Team: GE red
 // Role: Frontend developer
 // TA: Surabhi
 // Lecturer: Florian
 // Notes to Grader: <optional extra notes>
-
 public interface FrontendInterface {
     public void run(Backend backend);
 }
